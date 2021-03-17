80bc959a5c5836fc3fd9893708029799e75aac97
senaarth <senaarth@gmail.com>
2021-03-17 17:37:38 -0300

    creating and connecting to fake api

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: "Transação Feita",
+          amount: 400,
+          type: "deposit",
+          category: "Categoria de Transação",
+          created_at: new Date()
+        }
+      ]
+    })
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
